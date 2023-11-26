f3a04f2d4743e6be6749f0240f4ba42a4947a2eb
Tereza Smolova <tereza.smolova@gmail.com>
2023-11-26 16:56:03 +0100

    Switch button

--- src/components/SwitchButtons/index.jsx
+++ src/components/SwitchButtons/index.jsx
@@ -1,24 +1,53 @@
 import "./style.scss";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import imgSwichBase from "./img/posouvatko_zaklad.svg"
-import imgSwichHome from "./img/posouvatko_domu.svg"
-import imgSwichInfo from "./img/posouvatko_info.svg"
+import imgSwitchBase from "./img/posouvatko_zaklad.svg"
+import imgSwitchHome from "./img/posouvatko_domu.svg"
+import imgSwitchInfo from "./img/posouvatko_info.svg"
 
 
 export const SwitchButtons = () => {
+  const [switchButton, setSwitchButton] = useState(null)
+  const [startPosition, setStartPosition] = useState(null);
+
+  const handleTouchStart = (event) => {
+    setStartPosition(event.touches[0].clientX);
+  };
+
+  const handleTouchMove = (event) => {
+    if (startPosition !== null) {
+      const currentPosition = event.touches[0].clientX;
+      const deltaPosition = currentPosition - startPosition;
+
+      // omezení pohybu doleva nebo doprava
+      const newLeftPosition = Math.min(1, Math.max(0, 15 + deltaPosition / window.innerWidth * 100));
+
+      setSwitchButton(newLeftPosition);
+    }
+  };
+
+  const handleTouchEnd = () => {
+    setStartPosition(null);
+  };
+
   return (
     <>
       <div className="switch-button switch-button--1">
-        <img className="switch-button__base" src={imgSwichBase} alt="ikona posouvátko" />
+        <img className="switch-button__base" src={imgSwitchBase} alt="ikona posouvátko" />
         <Link to="/vyber-zviratka" >
-          <img className="switch-button__home" src={imgSwichHome} alt="ikona domů" />
+          <img onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
+            onTouchEnd={handleTouchEnd} 
+            style={{ left: `${switchButton}vmin` }}
+            className="switch-button__home" 
+            src={imgSwitchHome} alt="ikona domů" />
         </Link>
       </div>
 
       <div className="switch-button switch-button--2">
-        <img className="switch-button__base" src={imgSwichBase} alt="ikona posouvátko" />
+        <img className="switch-button__base" src={imgSwitchBase} alt="ikona posouvátko" />
         <Link to="/info" >
-          <img className="switch-button__info" src={imgSwichInfo} alt="ikona info" />
+          <img className="switch-button__info" src={imgSwitchInfo} alt="ikona info" />
         </Link>
       </div>
     </>
